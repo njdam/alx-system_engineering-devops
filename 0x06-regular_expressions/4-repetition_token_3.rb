3c11e379885ee84d8c0b252f674120939d2da57a
njdam <jeandamngaboyimana@gmail.com>
2023-08-01 19:13:24 +0000

    A Ruby script that accepts one argument for Repetition Token #3.

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# A Ruby script for Repetition Token #4
+puts ARGV[0].scan(/hbt{0,}n/).join
---
+++ 0x06-regular_expressions/example.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# An example of ruby script.
+puts ARGV[0].scan(/127.0.0.[0-9]/).join
