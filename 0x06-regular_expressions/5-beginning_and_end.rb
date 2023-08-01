4ea24b21dc7596f8db56c903a24c55c1cecae474
njdam <jeandamngaboyimana@gmail.com>
2023-08-01 20:21:06 +0000

    A Ruby script for Not quite HBTN yet.

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -2,4 +2,4 @@
 # A Ruby script that check if the argument matches the regular expression
 # for Not quite HBTN yet (case-sensitive).
 
-puts ARGV[0].match(/\Ah[b?8]n\z/)
+puts ARGV[0].match(/^h.n$/)
