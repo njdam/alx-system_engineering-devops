d9246d3b9222cb4924a24d889c519ded3b106a7a
njdam <jeandamngaboyimana@gmail.com>
2023-10-23 16:04:34 +0000

    0. Gather data from an API.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,36 +4,35 @@
 import requests
 from sys import argv, exit
 
-# Checking number of arguments
-if len(argv) != 2:
-    print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-    exit(1)
 
-# Employee Id
-employee_id = argv[1]
-# Api url where to request information
-base_url = 'https://jsonplaceholder.typicode.com'
+if __name__ == '__main__':
+    # Checking number of arguments
+    if len(argv) > 1:
+        # Employee Id
+        employee_id = argv[1]
+        # Api url where to request information
+        base_url = 'https://jsonplaceholder.typicode.com'
 
-# response user and todos by employee id
-user = requests.get(f'{base_url}/users/{employee_id}')
-todos = requests.get(f'{base_url}/todos?userId={employee_id}')
+        # response user and todos by employee id
+        user = requests.get(f'{base_url}/users/{employee_id}')
+        todos = requests.get(f'{base_url}/todos?userId={employee_id}')
 
-# Checking status of requested employee id, failed to be fetched
-if user.status_code != 200 or todos.status_code != 200:
-    print(f'Failed to fetch data for employee {employee_id}')
-    exit(1)
+        # Checking status of requested employee id, failed to be fetched
+        if user.status_code != 200 or todos.status_code != 200:
+            print(f'Failed to fetch data for employee {employee_id}')
+            exit(1)
 
-# User data into json file
-user_data = user.json()
-todos_data = todos.json()
+        # User data into json file
+        user_data = user.json()
+        todos_data = todos.json()
 
-# Tasks completed
-tasks_completed = [todo for todo in todos_data if todo['completed']]
-total_tasks = len(todos_data)
+        # Tasks completed
+        tasks_completed = [todo for todo in todos_data if todo['completed']]
+        total_tasks = len(todos_data)
 
-# Printing TODOs list progress of a given employ id
-print(f"Employee {user_data['name']} is done with tasks ", end="")
-print(f"({len(tasks_completed)}/{total_tasks}):")
+        # Printing TODOs list progress of a given employ id
+        print(f"Employee {user_data['name']} is done with tasks ", end="")
+        print(f"({len(tasks_completed)}/{total_tasks}):")
 
-for task in tasks_completed:
-    print(f"\t{task['title']}")
+        for task in tasks_completed:
+            print(f"\t{task['title']}")
