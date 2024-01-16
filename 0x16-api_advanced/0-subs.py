6fe0fbc91b3592b70701a88c4e447aa7557bde7a
njdam <jeandamngaboyimana@gmail.com>
2024-01-16 22:49:44 +0000

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-""" A function to querry Reddit API and return number of subscribers. """
-
+'''A function to querry Reddit API and return number of subscribers.
+'''
 import requests
 
 
+BASE_URL = 'https://www.reddit.com'
+'''A reddit's base API URL.
+'''
+
+
 def number_of_subscribers(subreddit):
-    """ A function to return total number of subscribers of a subreddit."""
-    URL = 'https://www.reddit.com'
-
+    '''A function to return total number of subscribers of a subreddit.
+    '''
     api_headers = {
-            'Accept': 'application/json',
-            'User-Agent': 'CustomUserAgent'
-            }
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
     response = requests.get(
-            '{}/r/{}/about/.json'.format(URL, subreddit),
-            headers=api_headers,
-            allow_redirects=False
-            )
-    results = response.json()
-
+        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
     if response.status_code == 200:
-        return (results['data']['subscribers'])
-    return (0)
+        return response.json()['data']['subscribers']
+    return 0
