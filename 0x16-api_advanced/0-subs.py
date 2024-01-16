6ce2702b48613839ffe4a8c9872cce82d9104230
njdam <jeandamngaboyimana@gmail.com>
2024-01-16 17:05:54 +0000

    Fixed 0.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """ A function to querry Reddit API and return number of subscribers. """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """ A function to return total number of subscribers of a subreddit. """
-
     # Reddit API to get subreddit information
     api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     # Setting custom user agent to avoid many request errors
-    headers = {'UserAgent': 'CustomUserAgent'}
+    # headers = {'User-Agent': 'CustomUserAgent'}
 
     # Api request
     response = requests.get(api_url, headers=headers, allow_redirects=False)
