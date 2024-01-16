608acb90f3589894a4a6c2ad65df6334bec16898
njdam <jeandamngaboyimana@gmail.com>
2024-01-16 17:22:47 +0000

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     # Setting custom user agent to avoid many request errors
-    headers = {'User-Agent': 'CustomUserAgent'}
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
 
     # Api request
     response = requests.get(url, headers=headers, allow_redirects=False)
