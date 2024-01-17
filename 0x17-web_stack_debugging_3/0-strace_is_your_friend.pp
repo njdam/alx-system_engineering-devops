92ffd82e20508aa9c14962da6c4417883f428f32
njdam <jeandamngaboyimana@gmail.com>
2024-01-17 16:45:47 +0000

    0. Strace is your friend.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,27 +1,6 @@
-# Defining a Puppet exec resources to run strace on the Apache process
-exec { 'strace_apache':
-  command	=> 'strace -p $(pgrep apache2) -o /tmp/strace_output.txt',
-  path		=> '/bin:/usr/bin',
-  user		=> 'root',
-  require	=> Package['strace'],
-}
-
 # Defining a Puppet exec resource to fix the identified issue
 exec { 'fix-wordpress':
-  command 	=> 'sudo service apache2 restart',
-  path		=> '/bin:/usr/bin',
-  refreshonly	=> true,
-  subscribe	=> Exec['strace_apache'],
-}
-
-# To ensure that the 'strace' package is installed
-package { 'strace':
-  ensure 	=> present,
-}
-
-# Notify a service restart after fixing the issue
-service { 'apache2':
-  ensure 	=> running,
-  require	=> Exec['fix-wordpress'],
-  notify	=> Service['apache2'],
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
