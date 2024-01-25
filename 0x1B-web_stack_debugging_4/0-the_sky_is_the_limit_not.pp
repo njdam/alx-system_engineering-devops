9882adceef4498cd9ab5cf4f509546732883c808
njdam <jeandamngaboyimana@gmail.com>
2024-01-25 10:12:44 +0000

    0. Sky is the limit, let's bring that limit higher.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Scripts for fixing nginx failed to handle concurrent requests
 
 exec { 'fix--for-nginx':
-  command => "bash -c \"sed -iE 's/^ULIMIT=.*:/ULIMIT=\\\"\'-n 8192\\\"/' \
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
 /etc/default/nginx; service nginx restart\"",
   path    => '/usr/bin:/usr/sbin:/bin'
 }
